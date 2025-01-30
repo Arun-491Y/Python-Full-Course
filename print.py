0bde56d3c4b71062366d4678ef6542c133e427f0
Arun-491Y <arunkgisl491@gmail.com>
2025-01-30 11:47:48 +0530

    successfully started the course by printing values

---
+++ print.py
@@ -0,0 +1,10 @@
+#Print arguments
+print('2+3', end="=")#end statement gives the out print in same line 
+print(2+3)
+print("college","lab","students","pyhton", sep='.')#it is used to seperate the variables in between the gap
+#---------------------------------
+print("escape\nsequence")#\n is used to make the sentence or a variable to move to the next line(simply it escape to next line)
+print("escape\tsequence")#\t is used to make space in between the variables
+print("gukan said,\"this is my phone\" ")# \ it is used to add the quotes in between the print state or used to understand the program to ' is included in that particular statement
+print("gukan said,'this is my phone'")#Pytrick it simple way to add quotes in the printing sentences  or alternative method
+print("Its hero time \n\t\t-BEN-10")
