ad24fe644778a9851ee0c16cf1151463a38687df
Arun-491Y <arunkgisl491@gmail.com>
2025-01-30 15:54:13 +0530

    successfully completed datatypes

--- data_types.py
+++ data_types.py
@@ -1,7 +1,12 @@
-#Basic datatypes
+'Basic datatypes'
 name="Arun"#string
 age=19#integer
 balance=82.1#float
 hascreditcard=True#boolean
 print(name,age,balance,hascreditcard)
 #Keyword should not be an indentifier,all identifiers are case sensitive.
+print(type(2))#it is used to find the type of data 
+name,age = "Arun",19 #To enter multi datatypes in a single line 
+'Indexing'
+print(name[0])#display the index of the string
+print(name[3])
