7bd9697f1873963f5bcbd364d199fda9ba382b55
Arun-491Y <arunkgisl491@gmail.com>
2025-02-01 20:06:57 +0530

    compeleted relational or comparitive operator.

---
+++ Relational_opeator.py
@@ -0,0 +1,16 @@
+a=20
+b=15
+print(a<b)#less than 
+print(a>b)#greater than 
+print(a<=b)#less than or equal to
+print(a>=b)#greater than or equal to
+print(a!=b)#not equal to 
+print(a==b)#equal to 
+a="Siva"
+b="siva"
+print(a==b)#false because capital string is not equal to small 
+print(a<b)#True ,here the ascii value is not same for S and s
+#to find the asciie value 
+# ord is used to find the asciie value  
+print('s:',ord('s'))#value is 115
+print('S:',ord('S'))#value is 83
