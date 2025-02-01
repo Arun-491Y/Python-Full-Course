790a962fb7adbcc4bb5c1e886a96ac09b0bb64f6
Arun-491Y <arunkgisl491@gmail.com>
2025-02-01 20:29:20 +0530

    completed agumented operator successfully

---
+++ agumented_operator.py
@@ -0,0 +1,11 @@
+#Agumented assignment operator
+#the combination of assignment and relational operator is called Agumented assignment operator 
+a=10
+a+=2#adding new value with old value of a (a=10+2)
+print(a)
+a-=2#subtraction
+print(a)
+a*=2#multiplication
+print(a)
+a/=2#division 
+print(a)
