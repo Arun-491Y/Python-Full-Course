b8c0fba97756887077b2fde30d53150a79ea78ef
Arun-491Y <arunkgisl491@gmail.com>
2025-02-02 21:48:42 +0530

    completed membership and logiacal operator

--- Logical operator.py
+++ Logical operator.py
@@ -5,3 +5,4 @@ st2=(b==10)
 print(st1 or st2)#OR operator is used to  find either a is true or b is true ,if anyone of these two is true return true 
 print(st1 and st2)#both a and b are ture
 print(not(False))# not operator is used to print the opposite value 
+#note:when and & or operators in the operation,if the first value satisfy the condition it does not check the second value  
