b4aa95a03d40b105b88ef028c8abd74d5770b4fd
Arun-491Y <arunkgisl491@gmail.com>
2025-02-02 21:44:29 +0530

    completed membership operator

---
+++ Logical operator.py
@@ -0,0 +1,7 @@
+a=10
+b=5
+st1=(a==10)
+st2=(b==10)
+print(st1 or st2)#OR operator is used to  find either a is true or b is true ,if anyone of these two is true return true 
+print(st1 and st2)#both a and b are ture
+print(not(False))# not operator is used to print the opposite value 
---
+++ Membership operator.py
@@ -0,0 +1,6 @@
+'Membership ,Check if an element is a member'
+lst=[1,2,3,4,5]
+name = 'Arun'
+print('A' in name)
+print(5 in lst)#Check the integer 5 is in the list 
+print(9 not in lst)#check weather the int 9 is not in lst 
